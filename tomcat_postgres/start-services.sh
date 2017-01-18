3df46c61c6224bacba49d25aecefcf65c83b7679
dinar-dalvi <dinardalvi@yahoo.com>
2017-01-17 17:40:11 -0800

    new images

--- tomcat_postgres/start-services.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-# Launch Tomcat
-/opt/tomcat/bin/catalina.sh run &
-
-#CMD ["/usr/lib/postgresql/9.3/bin/postgres", "-D", "/var/lib/postgresql/9.3/main", "-c", "config_file=/etc/postgresql/9.3/main/postgresql.conf"]
-#launch Postgres
-
