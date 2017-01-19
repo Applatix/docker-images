1129b4738eb9a6ddd67e3d09982d9728ab4f3ff4
dinar-dalvi <dinardalvi@yahoo.com>
2017-01-18 23:19:29 -0800

    fixed default run

--- tomcat-postgres/run.sh
+++ tomcat-postgres/run.sh
@@ -11,17 +11,19 @@ echo "starting postgreSQL ... "
 
 # CMD ["/usr/lib/postgresql/9.3/bin/postgres", "-D", "/var/lib/postgresql/9.3/main", "-c", "config_file=/etc/postgresql/9.3/main/postgresql.conf"]
 
-su postgres -c "pg_ctl -D $DATA_DIR/data -l ${DATA_DIR}/logfile start"
+sudo service postgresql restart
+
+#postgres -c "pg_ctl -D $DATA_DIR/data -l ${DATA_DIR}/logfile start"
 
 echo "started postgreSQL"
 
 #start tomcat
 echo "starting tomcat ... "
 
-echo "export JAVA_OPTS=\"-Dapp.env=dev\"" > /usr/local/tomcat/bin/setenv.sh  
+#echo "export JAVA_OPTS=\"-Dapp.env=dev\"" > /usr/local/tomcat/bin/setenv.sh
+#sudo sh /opt/tomcat/bin/catalina.sh stop
 
-./opt/tomcat/bin/catalina.sh &
-
+sudo sh /opt/tomcat/bin/catalina.sh start
 
 
 echo "started tomcat"
