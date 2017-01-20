4c8a3efe35e50423930c5fce99e76d6a5f8d2b9e
dinar-dalvi <dinardalvi@yahoo.com>
2017-01-19 22:51:02 -0800

    Code complete

--- tomcat-postgres/run.sh
+++ tomcat-postgres/run.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
 # Stop on error
-sudo set -e
+set -e
 
-# Variables
-#DATA_DIR=/var/lib/postgresql
 
 #start postgreSQL
 echo "starting postgreSQL ... "
 
-# CMD ["/usr/lib/postgresql/9.3/bin/postgres", "-D", "/var/lib/postgresql/9.3/main", "-c", "config_file=/etc/postgresql/9.3/main/postgresql.conf"]
-
 sudo service postgresql restart
 
-#postgres -c "pg_ctl -D $DATA_DIR/data -l ${DATA_DIR}/logfile start"
-
 echo "started postgreSQL"
+
+echo "sleep 45 seconds to give DB the time to start"
+
+sleep 45
+
+echo "running insert data"
+
+psql npgeek postgres -f /tmp/init.sql 
+
+echo "running insert data complete"
 
 #start tomcat
 echo "starting tomcat ... "
 
-#echo "export JAVA_OPTS=\"-Dapp.env=dev\"" > /usr/local/tomcat/bin/setenv.sh
-#sudo sh /opt/tomcat/bin/catalina.sh stop
+#sh /opt/tomcat/bin/catalina.sh stop
 
-sudo sh /opt/tomcat/bin/catalina.sh start
+sh /opt/tomcat/bin/catalina.sh start
 
 
 echo "started tomcat"
