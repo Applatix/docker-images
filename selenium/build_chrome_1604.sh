93968ac3196a7ddcb9c9310bb54bd43b908bf15f
dinar-dalvi <dinardalvi@yahoo.com>
2017-02-14 16:54:02 -0800

    fixed broken images

--- selenium/build_chrome_1604.sh
+++ selenium/build_chrome_1604.sh
@@ -19,33 +19,33 @@
 
 #sleep 3
 
-#cd /Users/dinar_dalvi/applatix_code/docker-images/selenium/ubuntu-16.04-chrome/chrome-50
+cd /Users/dinar_dalvi/applatix_code/docker-images/selenium/ubuntu-16.04-chrome/chrome-50
 
-#docker build -t applatix/ubuntu1604-chrome50 .
+docker build -t applatix/ubuntu1604-chrome50 .
 
-#sleep 3
+sleep 3
 
-#cd /Users/dinar_dalvi/applatix_code/docker-images/selenium/ubuntu-16.04-chrome/chrome-51
+cd /Users/dinar_dalvi/applatix_code/docker-images/selenium/ubuntu-16.04-chrome/chrome-51
 
-#docker build -t applatix/ubuntu1604-chrome51 .
+docker build -t applatix/ubuntu1604-chrome51 .
 
-#sleep 3
+sleep 3
 
-#cd /Users/dinar_dalvi/applatix_code/docker-images/selenium/ubuntu-16.04-chrome/chrome-52
+cd /Users/dinar_dalvi/applatix_code/docker-images/selenium/ubuntu-16.04-chrome/chrome-52
 
-#docker build -t applatix/ubuntu1604-chrome52 .
+docker build -t applatix/ubuntu1604-chrome52 .
 
-#sleep 3
+sleep 3
 
-#cd /Users/dinar_dalvi/applatix_code/docker-images/selenium/ubuntu-16.04-chrome/chrome-53
+cd /Users/dinar_dalvi/applatix_code/docker-images/selenium/ubuntu-16.04-chrome/chrome-53
 
-#docker build -t applatix/ubuntu1604-chrome53 .
+docker build -t applatix/ubuntu1604-chrome53 .
 
-#sleep 3
+sleep 3
 
-#cd /Users/dinar_dalvi/applatix_code/docker-images/selenium/ubuntu-16.04-chrome/chrome-54
+cd /Users/dinar_dalvi/applatix_code/docker-images/selenium/ubuntu-16.04-chrome/chrome-54
 
-#docker build -t applatix/ubuntu1604-chrome54 .
+docker build -t applatix/ubuntu1604-chrome54 .
 
 #sleep 3
 
@@ -57,9 +57,9 @@
 
 echo "done building now uploading "
 
-docker push applatix/ubuntu1604-chrome49
+#docker push applatix/ubuntu1604-chrome49
 
-sleep 5
+#sleep 5
 
 docker push applatix/ubuntu1604-chrome50
 
